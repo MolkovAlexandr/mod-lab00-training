e8384439d0e51f5265190fd78e40f00a868e85b9
MolkovAlexandr <163853240+MolkovAlexandr@users.noreply.github.com>
2024-04-23 17:03:41 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
-int main() {
-    return 0;
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "1251");
+
+	int a = 0, b = 0;
+
+	cout << "Введите x: "; cin >> a;
+	cout << "Введите n: "; cin >> b;
+
+	cout << power(a, b);
+
+	return 0;
 }
